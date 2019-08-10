32b464aa7700f0ec982f2a6831ef6296d74d6500
bmolly <bmolly94@hotmail.com>
2019-08-10 15:56:07 -0700

    modifing blog app

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+posts = [
+	{
+		'author': 'MollyMS',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'August 10, 2019'
+	},
+	{
+		'author': 'Ivy Zhu',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'August 10, 2019'
+	}
+]
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render(request,'blog/home.html',context)
+
+def about(request):
+	return render(request,'blog/about.html', {'title': 'About'})
