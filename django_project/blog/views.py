c2c0ba77c4871c37ddc13bce4bc1f2fcc40abb9e
bmolly <bmolly94@hotmail.com>
2019-08-11 16:31:11 -0700

    addming database, delete fake post data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-
-posts = [
-	{
-		'author': 'MollyMS',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'August 10, 2019'
-	},
-	{
-		'author': 'Ivy Zhu',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'August 10, 2019'
-	}
-]
-
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request,'blog/home.html',context)
 
